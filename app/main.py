df7747bad7878e3989c4f99e82012dfda4875b4d
Ali Hejazi <ahejazi1999@gmail.com>
2022-11-20 23:38:24 +0000

    CORS fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Response, Depends
-import jsonpickle
+from fastapi.middleware.cors import CORSMiddleware
 from models.user import User
 from models.error_response import ErrorResponse
 from exceptions.service_unavailable_exception import ServiceUnavailableException
 from dependencies import dependencies
+import jsonpickle
 
 # Run the app
 app = FastAPI()
@@ -13,6 +14,10 @@ app = FastAPI()
 # time-limitations I have not been able to implement any middleware in this submission, but I am
 # commenting here to demonstrate knowledge.
 # app.add_middleware(...)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000']
+)
 
 @app.get("/users/{user_id}")
 def getUser(user_id: int, resp: Response, dependencies: dict = Depends(dependencies)):
